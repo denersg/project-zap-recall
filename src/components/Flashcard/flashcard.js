6d64442c6adc6854c5fc29b978b172708cae1836
Dener Silva <dener.ufes@gmail.com>
2022-03-20 17:46:33 -0300

    Finalizada a escolha de resposta para os cards

--- src/components/Flashcard/flashcard.js
+++ src/components/Flashcard/flashcard.js
@@ -2,15 +2,55 @@ import { useState } from "react/cjs/react.development";
 import TurnIcon from "../../assets/turn-icon.svg";
 import "./styleflashcard.css";
 
-export default function FlashCard({ index, question, answer }){
-    const [isFlipped, setIsFlipped] = useState(false);
+function Footer({ answerFlashCard }){
+    return(
+        <div className="footer">
+            <button className="incorrect" onClick={() => answerFlashCard("incorrect")}>Não lembrei</button>
+            <button className="correct" onClick={() => answerFlashCard("correct")}>Quase não lembrei</button>
+            <button className="zap" onClick={() => answerFlashCard("zap")}>Zap!</button>
+        </div>
+    );
+}
 
-    if(isFlipped){
+export default function FlashCard({ index, question, answer }){
+    const [isTurnedToQuestion, setIsTurnedToQuestion] = useState(false);
+    const [isTurnedToResponse, setIsTurnedToResponse] = useState(false);
+    const [isAnswered, setIsAnswered] = useState(false);
+    const [status, setStatus] = useState("");
+    // console.log(isTurnedToResponse)
+
+    function answerFlashCard(status){
+        setStatus(status);
+        setIsAnswered(true);
+        //Criar a função de acançar a posição
+    }
+
+    if(isAnswered){
+        console.log("Status: "+status)/*Só imprime o status depois
+                                        que o botão foi escolhido!*/
+        return(
+            <div className="answered-card">
+                <span>{index}</span>
+                <ion-icon name="checkmark-circle"></ion-icon>
+            </div>
+        );
+    }
+
+    if(isTurnedToResponse){
+        return(
+            <div className="flashcard-response">
+                <span>{answer}</span>
+                <Footer answerFlashCard={answerFlashCard} />
+            </div>
+        );
+    }
+
+    if(isTurnedToQuestion){
         return(
             <div className="flashcard-question">
-                <span>{question} kkjnak oniaxb cksbxa biwxw bicbw cbksab</span>
+                <span>{question}</span>
                 <div className="turn-button">
-                    <img src={TurnIcon} alt="virar" />
+                    <img src={TurnIcon} alt="virar" onClick={() => setIsTurnedToResponse(true) } />
                 </div>
             </div>
         );
@@ -19,7 +59,7 @@ export default function FlashCard({ index, question, answer }){
     return(
         <div>
             
-            <p onClick={() => setIsFlipped(true) }>{index}<ion-icon name="play-outline"></ion-icon></p>
+            <p onClick={() => setIsTurnedToQuestion(true) }>{index}<ion-icon name="play-outline"></ion-icon></p>
             
         </div>
     );
